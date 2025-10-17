9f4d4dac1077db5de81f28dfd1a4b475765a6573
Ketan Thombare <ketancode14@gmail.com>
2025-10-18 01:55:11 +0530

    Updated meta data as well

--- app/metadata.ts
+++ app/metadata.ts
@@ -1,8 +1,17 @@
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "Ketan's Portfolio",
-  description: "Modern & Minimalistic Portfolio",
+  title: "Ketan Thombare | Full-Stack Software Developer",
+  description: "I’m Ketan Thombare, a self-taught full-stack developer. From building scalable web apps to leading tech communities, I turn ideas into meaningful digital solutions.",
+  openGraph: {
+    title: "Ketan Thombare – Building Scalable & Intelligent Web Experiences",
+    description: "I’m Ketan Thombare, a self-taught full-stack developer turning ideas into meaningful digital solutions.",
+  },
+  twitter: {
+    title: "Ketan Thombare – Building Scalable & Intelligent Web Experiences",
+    description: "I’m Ketan Thombare, a self-taught full-stack developer turning ideas into meaningful digital solutions.",
+    card: 'summary_large_image',
+  },
   icons: {
     icon: '/Icon.webp',
   },
